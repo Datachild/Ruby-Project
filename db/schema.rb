6b27a41e0c650f6d00f5b65da910b77d452ecd04
Aleksey Sergeyev <alekseysergeyev@gmail.com>
2016-12-09 13:06:49 -0500

    removing garbage from schema

--- db/schema.rb
+++ db/schema.rb
@@ -15,13 +15,4 @@ ActiveRecord::Schema.define(version: 20161209173138) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "players", force: :cascade do |t|
-    t.string   "first_name"
-    t.string   "last_name"
-    t.string   "position"
-    t.string   "picture"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
 end
