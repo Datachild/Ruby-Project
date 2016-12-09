e103087e5ac249c4e86f25e8e393cc254ee14e08
Aleksey Sergeyev <alekseysergeyev@gmail.com>
2016-12-09 18:16:59 +0000

    recreated players

---
+++ db/migrate/20161209181516_create_players.rb
@@ -0,0 +1,13 @@
+class CreatePlayers < ActiveRecord::Migration[5.0]
+  def change
+    create_table :players do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :card_type
+      t.string :position
+      t.string :picture
+
+      t.timestamps
+    end
+  end
+end
