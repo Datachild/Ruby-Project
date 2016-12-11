28f7db5cd0413493e597097bb4ea3abd17b77ffc
Aleksey Sergeyev <alekseysergeyev@gmail.com>
2016-12-11 12:54:41 -0500

    added create to controlelr

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -2,4 +2,18 @@ class PlayersController < ApplicationController
   def index
       @players = Player.all
     end
+  def create
+      @player = Player.new(player_params)
+
+      if @player.save
+        render json: @player
+      else
+        render json: @player.errors, status: :unprocessable_entity
+      end
+    end
+
+    private
+      def player_params
+        params.require(:player).permit(:first_name, :last_name, :position, :card_type)
+      end
 end
