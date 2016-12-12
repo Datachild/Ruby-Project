88d088f8a3d119035eee8f4a4e8352512fa2c397
Aleksey Sergeyev <alekseysergeyev@gmail.com>
2016-12-11 21:07:12 -0500

    fix ths fix and fix fkn comma

--- app/assets/javascripts/components/_players_form.js.jsx
+++ app/assets/javascripts/components/_players_form.js.jsx
@@ -6,8 +6,8 @@ var PlayersForm = React.createClass({
     var card_type = this.refs.card_type.value;
     $.ajax({
       url: '/api/v1/players',
-      type: 'POST'
-      data: {player:{ first_name: first_name, last_name: last_name, position: position, card_type: card_type }}
+      type: 'POST',
+      data: { player: { first_name: first_name, last_name: last_name, position: position, card_type: card_type } }
     })
   }
   render() {
