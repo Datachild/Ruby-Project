06678bbf097a4a5aec102f12a66c7add8a1e5b26
Aleksey Sergeyev <alekseysergeyev@gmail.com>
2016-12-11 19:53:49 -0500

    added header and footer components

---
+++ app/assets/javascripts/components/_footer.js.jsx
@@ -0,0 +1,11 @@
+// app/assets/javascripts/components/_header.js.jsx
+
+var Footer = React.createClass({
+  render() {
+    return (
+      <div>
+        <h1>Goodbye world</h1>
+      </div>
+    )
+  }
+});
