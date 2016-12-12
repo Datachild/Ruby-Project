8f8e2ede9769edede7921abf3ce1859715ef6786
Aleksey Sergeyev <alekseysergeyev@gmail.com>
2016-12-12 13:30:08 -0500

    f

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ module Testapp
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.assets.paths << Rails.root.join('vendor', 'assets', 'bower_components')
     config.react.addons = true
   end
 end
