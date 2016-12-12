2af32fc458b9103265281781906bea74421601aa
Aleksey Sergeyev <alekseysergeyev@gmail.com>
2016-12-12 13:26:47 -0500

    f

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,10 +12,10 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require lodash
 //= require turbolinks
 //= require react
 //= require react_ujs
 //= require components
 //= require_tree .
 //= require bootstrap-sprockets
-//= require lodash
