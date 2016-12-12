ccf3614cd7b04ac17f56aad022cbd4cb52781b09
Aleksey Sergeyev <alekseysergeyev@gmail.com>
2016-12-12 14:46:31 -0500

    t

--- app/assets/javascripts/components/_players_list.js.jsx
+++ app/assets/javascripts/components/_players_list.js.jsx
@@ -7,16 +7,16 @@ var PlayersList = React.createClass({
       sortOrder: 'asc'
     }
   },
-  sortPlayers(atr){
-    // var order = (atr != this.state.sortBy || this.state.sortOrder == 'desc')?'asc':'desc';
-    // this.setState({ sortBy: atr, sortOrder: order })
-  },
   componentDidMount() {
     $.getJSON('/api/v1/players.json', (response) => { this.setState({ players: response }) });
   },
   handleAddPlayer(player){
     var newState = this.state.players.concat(player);
     this.setState({ players: newState })
+  },
+  handleSortPlayers(atr){
+    var order = (atr != this.state.sortBy || this.state.sortOrder == 'desc')?'asc':'desc';
+    this.setState({ sortBy: atr, sortOrder: order })
   },
   handleEditPlayer(oldPlayer,player){
     $.ajax({
@@ -58,10 +58,10 @@ var PlayersList = React.createClass({
           <tbody className="table-striped">
             <PlayersForm handleAddPlayer={this.handleAddPlayer} />
             <tr>
-              <th onClick={this.sortPlayers('first_name')}>First Name</th>
-              <th onClick={this.sortPlayers('last_name')}>Last Name</th>
-              <th onClick={this.sortPlayers('position')}>Position</th>
-              <th onClick={this.sortPlayers('type')}>Type</th>
+              <th onClick={this.handleSortPlayers('first_name')}>First Name</th>
+              <th onClick={this.handleSortPlayers('last_name')}>Last Name</th>
+              <th onClick={this.handleSortPlayers('position')}>Position</th>
+              <th onClick={this.handleSortPlayers('type')}>Type</th>
               <th>Options</th>
             </tr>
             {players}
