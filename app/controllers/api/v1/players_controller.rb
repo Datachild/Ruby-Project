b1950edfbb3a8bc0ee01e9f50b9becf258283d9a
Aleksey Sergeyev <alekseysergeyev@gmail.com>
2016-12-11 19:24:35 -0500

    added api controller ofr base and player

--- app/controllers/api/v1/players_controller.rb
+++ app/controllers/api/v1/players_controller.rb
@@ -1,32 +1,37 @@
-class PlayersController < ApplicationController
+class Api::V1::PlayersController < Api::V1::BaseController
   def index
-      @players = Player.all
-    end
-  def destroy
-      @player = Player.find(params[:id])
-      @player.destroy
-      head :no_content
-    end
-  def update
-      @player = Player.find(params[:id])
-      if @player.update(player_params)
-        render json: @player
-      else
-        render json: @player.errors, status: :unprocessable_entity
-      end
-    end
+    respond_with Player.all
+  end
+
   def create
-      @player = Player.new(player_params)
+    respond_with :api, :v1, Player.create(player_params)
+    # @player = Player.new(player_params)
+    #
+    # if @player.save
+    #   render json: @player
+    # else
+    #   render json: @player.errors, status: :unprocessable_entity
+    # end
+  end
 
-      if @player.save
-        render json: @player
-      else
-        render json: @player.errors, status: :unprocessable_entity
-      end
-    end
-
-    private
-      def player_params
-        params.require(:player).permit(:first_name, :last_name, :position, :card_type)
-      end
+  def destroy
+    respond_with :api, :v1, Player.destroy(params[:id])
+    # @player = Player.find(params[:id])
+    # @player.destroy
+    # head :no_content
+  end
+  def update
+    @player = Player.find(params[:id])
+    player.update_attributes(player_params)
+    respond_with player, json: player
+    # if @player.update(player_params)
+    #   render json: @player
+    # else
+    #   render json: @player.errors, status: :unprocessable_entity
+    # end
+  end
+  private
+  def player_params
+    params.require(:player).permit(:first_name, :last_name, :position, :card_type)
+  end
 end
