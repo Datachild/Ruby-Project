3a41b1e9922feb32e4d74c29e77a3692303449a5
Aleksey Sergeyev <alekseysergeyev@gmail.com>
2016-12-12 18:48:59 -0500

    added goals to routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :players, only: [:index, :create, :destroy, :update]
+      resources :goals, only: [:index, :create, :destroy, :update]
     end
   end
   # resources :players
