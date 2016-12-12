3478c49b170ba58aab41b2a88dffe037de280399
Aleksey Sergeyev <alekseysergeyev@gmail.com>
2016-12-11 19:26:57 -0500

    Adding routes for api players resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  resources :players
+  
+  namespace :api do
+    namespace :v1 do
+      resources :players, only: [:index, :create, :destroy, :update]
+    end
+  end
+  # resources :players
   root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
