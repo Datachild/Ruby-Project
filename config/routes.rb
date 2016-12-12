b13d35078959da48b25148b48094d658c60b902b
Aleksey Sergeyev <alekseysergeyev@gmail.com>
2016-12-11 19:36:33 -0500

    changed default controller and root rout

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  
+
   namespace :api do
     namespace :v1 do
       resources :players, only: [:index, :create, :destroy, :update]
     end
   end
   # resources :players
-  root 'welcome#index'
+  root 'site#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
