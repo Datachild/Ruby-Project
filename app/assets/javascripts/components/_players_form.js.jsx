1282964a079df7dd462ebde6b51b9b70493faa2a
Aleksey Sergeyev <alekseysergeyev@gmail.com>
2016-12-11 20:58:23 -0500

    added form functionality

--- app/assets/javascripts/components/_players_form.js.jsx
+++ app/assets/javascripts/components/_players_form.js.jsx
@@ -1,9 +1,21 @@
 var PlayersForm = React.createClass({
+  handleClick(){
+    var first_name = this.refs.first_name.value;
+    var last_name = this.refs.last_name.value;
+    var position = this.refs.position.value;
+    var card_type = this.refs.card_type.value;
+    $.ajax({
+      url: '/api/v1/players',
+      type: 'POST'
+      date: {player:{ first_name: first_name, last_name: last_name, position: position, card_type: card_type }},
+      // success: (response) => {}
+    })
+  }
   render() {
     return (
       <tr>
         <td><input ref="first_name" placeholder="First Name" /></td>
-        <td><input ref="first_name" placeholder="Last Name" /></td>
+        <td><input ref="last_name" placeholder="Last Name" /></td>
         <td>
           <select ref="position">
             <option value="">Position</option>
@@ -18,7 +30,7 @@ var PlayersForm = React.createClass({
           <option value="BRONZE">BRONZE</option>
         </select>
         </td>
-        <td><button>Create</button></td>
+        <td><button onClick={this.handleClick}>Create</button></td>
       </tr>
     )
   }
