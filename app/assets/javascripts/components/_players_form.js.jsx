7a25458ba66b8a1de29f412d2cdc3df56f214e9b
Aleksey Sergeyev <alekseysergeyev@gmail.com>
2016-12-11 21:20:52 -0500

    another comma

--- app/assets/javascripts/components/_players_form.js.jsx
+++ app/assets/javascripts/components/_players_form.js.jsx
@@ -1,15 +1,15 @@
 var PlayersForm = React.createClass({
-  // handleClick() {
-  //   var first_name = this.refs.first_name.value;
-  //   var last_name = this.refs.last_name.value;
-  //   var position = this.refs.position.value;
-  //   var card_type = this.refs.card_type.value;
-  //   $.ajax({
-  //     url: '/api/v1/players',
-  //     type: 'POST',
-  //     data: { player: { first_name: first_name, last_name: last_name, position: position, card_type: card_type } }
-  //   });
-  // }
+  handleClick() {
+    var first_name = this.refs.first_name.value;
+    var last_name = this.refs.last_name.value;
+    var position = this.refs.position.value;
+    var card_type = this.refs.card_type.value;
+    $.ajax({
+      url: '/api/v1/players',
+      type: 'POST',
+      data: { player: { first_name: first_name, last_name: last_name, position: position, card_type: card_type } }
+    });
+  },
   render() {
     return (
       <tr>
