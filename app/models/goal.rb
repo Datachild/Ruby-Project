b8406f9ee235b649b4886d90a17611a4672a1bec
Aleksey Sergeyev <alekseysergeyev@gmail.com>
2016-12-12 18:50:16 -0500

    f

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -1,5 +1,5 @@
 class Goal < ApplicationRecord
   belongs_to :goal_scorer, :class_name => 'Player'
-  belongs_to :assist1, :goal_assist1 => 'Player'
-  belongs_to :assist2, :goal_assist2 => 'Player'
+  belongs_to :goal_assist1, :class_name => 'Player'
+  belongs_to :goal_assist2, :class_name => 'Player'
 end
