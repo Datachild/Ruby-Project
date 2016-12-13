a432ff2bcded629ce85cc1bf4e473aaa68d52b38
Aleksey Sergeyev <alekseysergeyev@gmail.com>
2016-12-12 21:24:30 -0500

    changing models

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -1,5 +1,11 @@
 class Goal < ApplicationRecord
-  belongs_to :goal_scorer, :class_name => 'Player'
-  belongs_to :goal_assist1, :class_name => 'Player'
-  belongs_to :goal_assist2, :class_name => 'Player'
+  belongs_to  :goal_scorer,
+              :class_name => 'Player',
+              :foreign_key => 'goal_scorer'
+  belongs_to  :goal_assist1,
+              :class_name => 'Player',
+              :foreign_key => 'goal_assist1'
+  belongs_to  :goal_assist2,
+              :class_name => 'Player',
+              :foreign_key => 'goal_assist2'
 end
--- app/models/player.rb
+++ app/models/player.rb
@@ -1,2 +1,15 @@
 class Player < ApplicationRecord
+
+  has_many  :goals,
+            :class_name => 'Goal',
+            :foreign_key => 'goal_scorer'
+
+  has_many  :assists1,
+            :class_name => 'Goal',
+            :foreign_key => 'goal_assist1'
+
+  has_many  :assists2,
+            :class_name => 'Goal',
+            :foreign_key => 'goal_assist2'
+
 end
