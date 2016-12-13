1accc7d495ce4823165a0cd7ebfe9c1170197d6d
Aleksey Sergeyev <alekseysergeyev@gmail.com>
2016-12-13 13:20:04 -0500

    f

--- app/assets/javascripts/components/_header.js.jsx
+++ app/assets/javascripts/components/_header.js.jsx
@@ -6,14 +6,14 @@ var Header = React.createClass({
   },
   changeTab(tab){
     this.props.handlePageSwitch(tab);
-    this.refs.{"tm_"+this.state.activeTab}.className = '';
-    this.refs.{"tm_"+tab}.className = 'active';
+    $('#tm_'+this.state.activeTab).removeClass('active');
+    $('#tm_'+tab).addClass('active');
   },
   render() {
     return (
       <ul className="nav nav-tabs">
-        <li ref="tm_players" role="presentation" className="active" onClick={() => this.changeTab('players')}><a>Players</a></li>
-        <li ref="tm_goals" role="presentation" onClick={() => this.changeTab('goals')}><a>Goals</a></li>
+        <li id="tm_players" role="presentation" className="active" onClick={() => this.changeTab('players')}><a>Players</a></li>
+        <li id="tm_goals" role="presentation" onClick={() => this.changeTab('goals')}><a>Goals</a></li>
       </ul>
     )
   }
