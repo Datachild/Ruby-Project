11461d6c601fdd59a8f09f3c5b409d079067caa7
Aleksey Sergeyev <alekseysergeyev@gmail.com>
2016-12-13 12:05:33 -0500

    sad

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -1,5 +1,5 @@
 class Goal < ApplicationRecord
-  belongs_to  :plyers, :class_name => 'Player', :foreign_key => 'goal_scorer_id'
-  belongs_to  :goal_assist1, :class_name => 'Player', :foreign_key => 'goal_assist1_id'
-  belongs_to  :goal_assist2, :class_name => 'Player', :foreign_key => 'goal_assist2_id'
+  # belongs_to  :plyers, :class_name => 'Player', :foreign_key => 'goal_scorer_id'
+  # belongs_to  :goal_assist1, :class_name => 'Player', :foreign_key => 'goal_assist1_id'
+  # belongs_to  :goal_assist2, :class_name => 'Player', :foreign_key => 'goal_assist2_id'
 end
