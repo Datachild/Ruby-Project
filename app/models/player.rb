80270e7a2e06ec587775d2e8166b4debe5798ca1
Aleksey Sergeyev <alekseysergeyev@gmail.com>
2016-12-13 11:50:24 -0500

    f

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,5 +1,5 @@
 class Player < ApplicationRecord
-  has_many  :goals, :class_name => 'Goal', :foreign_key => 'goal_scorer_id'
-  has_many  :assists1, :class_name => 'Goal', :foreign_key => 'goal_assist1_id'
-  has_many  :assists2, :class_name => 'Goal', :foreign_key => 'goal_assist2_id'
+  has_many  :goal_scorer, :class_name => 'Goal', :foreign_key => 'goal_scorer_id'
+  has_many  :goal_assist1, :class_name => 'Goal', :foreign_key => 'goal_assist1_id'
+  has_many  :goal_assist2, :class_name => 'Goal', :foreign_key => 'goal_assist2_id'
 end
