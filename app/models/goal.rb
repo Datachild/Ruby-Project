b83f21bdfb3d7a17319be8a5bf563921102a2f61
Aleksey Sergeyev <alekseysergeyev@gmail.com>
2016-12-13 10:49:36 -0500

    try

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -2,4 +2,5 @@ class Goal < ApplicationRecord
   belongs_to  :goal_scorer, :class_name => 'Player', :foreign_key => 'goal_scorer_id'
   belongs_to  :goal_assist1, :class_name => 'Player', :foreign_key => 'goal_assist1_id'
   belongs_to  :goal_assist2, :class_name => 'Player', :foreign_key => 'goal_assist2_id'
+  has_many :players
 end
--- app/models/player.rb
+++ app/models/player.rb
@@ -1,5 +1,6 @@
 class Player < ApplicationRecord
-  has_many  :goals, :class_name => 'Goal', :foreign_key => 'goal_scorer'
-  has_many  :assists1, :class_name => 'Goal', :foreign_key => 'goal_assist1'
-  has_many  :assists2, :class_name => 'Goal', :foreign_key => 'goal_assist2'
+  # has_many  :goals, :class_name => 'Goal', :foreign_key => 'goal_scorer'
+  # has_many  :assists1, :class_name => 'Goal', :foreign_key => 'goal_assist1'
+  # has_many  :assists2, :class_name => 'Goal', :foreign_key => 'goal_assist2'
+  has_many  :goals
 end
