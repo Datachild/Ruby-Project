7c0e6681396858b41241d6ce9501663b6997f45f
Aleksey Sergeyev <alekseysergeyev@gmail.com>
2016-12-12 22:58:45 -0500

    f

--- app/assets/javascripts/components/_main.js.jsx
+++ app/assets/javascripts/components/_main.js.jsx
@@ -1,11 +1,15 @@
 // app/assets/javascripts/components/_main.js.jsx
 
 var Main = React.createClass({
+  getInitialState(){
+    return {page: '<Players />'}
+  },
   render() {
+    var page = this.state.page;
     return (
       <div>
         <Header />
-        <Players />
+        {page}
         <Footer />
       </div>
     )
