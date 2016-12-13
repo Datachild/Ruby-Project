862e760e82e61d14cc5fba7406b8a22e6b7959d8
Aleksey Sergeyev <alekseysergeyev@gmail.com>
2016-12-13 11:21:54 -0500

    f

--- app/controllers/api/v1/players_controller.rb
+++ app/controllers/api/v1/players_controller.rb
@@ -4,7 +4,7 @@ class Api::V1::PlayersController < Api::V1::BaseController
   end
 
   def show
-    respond_with Player.filter(params["id"])
+    respond_with Player.find(params["id"])
   end
 
   def create
