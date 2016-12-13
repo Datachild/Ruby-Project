f77a2cc09d77612fe9216cfbcd8ca1b91f84e1bb
Aleksey Sergeyev <alekseysergeyev@gmail.com>
2016-12-12 20:28:46 -0500

    f

--- app/controllers/api/v1/goals_controller.rb
+++ app/controllers/api/v1/goals_controller.rb
@@ -4,32 +4,17 @@ class Api::V1::GoalsController < Api::V1::BaseController
   end
 
   def create
-    respond_with :api, :v1, Goal.create(player_params)
-    # @player = Player.new(player_params)
-    #
-    # if @player.save
-    #   render json: @player
-    # else
-    #   render json: @player.errors, status: :unprocessable_entity
-    # end
+    respond_with :api, :v1, Goal.create(goal_params)
   end
 
   def destroy
     respond_with :api, :v1, Goal.destroy(params[:id])
-    # @player = Player.find(params[:id])
-    # @player.destroy
-    # head :no_content
   end
 
   def update
     goal = Goal.find(params["id"])
     goal.update_attributes(goal_params)
     respond_with goal, json: goal
-    # if @player.update(player_params)
-    #   render json: @player
-    # else
-    #   render json: @player.errors, status: :unprocessable_entity
-    # end
   end
   private
   def goal_params
