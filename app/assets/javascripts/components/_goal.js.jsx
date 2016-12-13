462f49873487f45838cccc04628f23ae3a010a4a
Aleksey Sergeyev <alekseysergeyev@gmail.com>
2016-12-12 22:54:34 -0500

    ading goals

---
+++ app/assets/javascripts/components/_goal.js.jsx
@@ -0,0 +1,62 @@
+var Player = React.createClass({
+  getInitialState(){
+    return {editable: false}
+  },
+  // toggleEdit(){
+  //   this.setState({ editable: !this.state.editable });
+  // },
+  // handleEditPlayer(){
+  //   var first_name = this.refs.first_name.value;
+  //   var last_name = this.refs.last_name.value;
+  //   var position = this.refs.position.value;
+  //   var card_type = this.refs.card_type.value;
+  //   var player = {id: this.props.player.id, first_name: first_name, last_name: last_name, position: position, card_type: card_type};
+  //   this.props.handleEditPlayer(this.props.player,player);
+  //   this.toggleEdit();
+  // },
+  render(){
+    return(this.state.editable? this.renderEditable() : this.renderNonEditable())
+  },
+  renderEditable(){
+    return()
+    // return(
+    //   <tr>
+    //     <td><input ref="first_name" className="form-control" type='text' defaultValue={this.props.player.first_name} /></td>
+    //     <td><input ref="last_name" className="form-control" type='text' defaultValue={this.props.player.last_name} /></td>
+    //     <td>
+    //       <select ref="position" className="form-control" defaultValue={this.props.player.position}>
+    //         <option value="">Position</option>
+    //         <option value="F">F</option>
+    //         <option value="D">D</option>
+    //       </select>
+    //     </td>
+    //     <td>
+    //       <select ref="card_type" className="form-control" defaultValue={this.props.player.card_type}>
+    //         <option value="">Type</option>
+    //         <option value="GOLD">GOLD</option>
+    //         <option value="BRONZE">BRONZE</option>
+    //       </select>
+    //     </td>
+    //     <td>
+    //       <button type="button" className="btn btn-success" onClick={this.handleEditPlayer}>Save</button>
+    //       <button type="button" className="btn btn-warning" onClick={this.toggleEdit}>Cancel</button>
+    //     </td>
+    //   </tr>
+    // )
+  },
+  renderNonEditable(){
+    return(
+      <tr>
+        <td>{this.props.goal.goal_scorer.first_name} {this.props.goal.goal_scorer.last_name}</td>
+        <td>{this.props.goal.goal_assist1.first_name} {this.props.goal.goal_assist1.last_name}</td>
+        <td>{this.props.goal.goal_assist2.first_name} {this.props.goal.goal_assist2.last_name}</td>
+        <td>{this.props.goal.period}</td>
+        <td>{this.props.goal.goal_type}</td>
+        <td>
+          <button type="button" className="btn btn-info">Edit</button>
+          <button type="button" className="btn btn-danger">Delete</button>
+        </td>
+      </tr>
+    )
+  }
+});
---
+++ app/assets/javascripts/components/_goals_list.js.jsx
@@ -0,0 +1,70 @@
+var GoalsList = React.createClass({
+  getInitialState() {
+    return {
+      goals: [],
+      sortBy: 'created_at',
+      sortOrder: 'asc'
+    }
+  },
+  componentDidMount() {
+    $.getJSON('/api/v1/goals.json', (response) => { this.setState({ goals: response }) });
+  },
+  // handleAddPlayer(player){
+  //   var newState = this.state.players.concat(player);
+  //   this.setState({ players: newState })
+  // },
+  // handleSortPlayers(atr){
+  //   var order = (atr != this.state.sortBy || this.state.sortOrder == 'desc')?'asc':'desc';
+  //   this.setState({ sortBy: atr, sortOrder: order })
+  // },
+  // handleEditPlayer(oldPlayer,player){
+  //   $.ajax({
+  //     url: `/api/v1/players/${player.id}`,
+  //     type: 'PUT',
+  //     data: {player: player},
+  //     success:() => {
+  //       var index = this.state.players.indexOf(oldPlayer);
+  //       var players = this.state.players;
+  //       players.splice(index,1,player);
+  //       this.setState({players: players});
+  //     }
+  //   });
+  // },
+  // handleDeletePlayer(id) {
+  //   $.ajax({
+  //     url: `/api/v1/players/${id}`,
+  //     type: 'DELETE',
+  //     success:()  => {
+  //       var newState = this.state.players.filter((player) => {
+  //         return player.id != id;
+  //       });
+  //       this.setState({ players: newState });
+  //     }
+  //   });
+  // },
+  render() {
+    var goals = _.orderBy(this.state.goals, this.state.sortBy, this.state.sortOrder);
+    goals = goals.map((goal) => {
+      return(
+        <Goal key={goal.id} goal={goal} />
+      )
+    })
+    return (
+      <div>
+        <table className="table">
+          <tbody className="table-striped">
+            <tr>
+              <th>Scorer</th>
+              <th>Assist</th>
+              <th>Assist</th>
+              <th>Period</th>
+              <th>Type</th>
+              <th>Options</th>
+            </tr>
+            {goals}
+          </tbody>
+        </table>
+      </div>
+    )
+  }
+});
