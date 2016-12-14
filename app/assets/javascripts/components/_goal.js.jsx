1b41ff37d4b92c569d2dcf703f48f6bae10e7590
Aleksey Sergeyev <alekseysergeyev@gmail.com>
2016-12-14 12:44:53 -0500

    f

--- app/assets/javascripts/components/_goal.js.jsx
+++ app/assets/javascripts/components/_goal.js.jsx
@@ -47,7 +47,7 @@ var Goal = React.createClass({
             <option value="">Period</option>
             <option value="1">1</option>
             <option value="2">2</option>
-            <option value="2">3</option>
+            <option value="3">3</option>
             <option value="4">4</option>
             <option value="5">5</option>
           </select>
--- app/assets/javascripts/components/_goals_form.js.jsx
+++ app/assets/javascripts/components/_goals_form.js.jsx
@@ -50,7 +50,7 @@ var GoalsForm = React.createClass({
             <option value="">Period</option>
             <option value="1">1</option>
             <option value="2">2</option>
-            <option value="2">3</option>
+            <option value="3">3</option>
             <option value="4">4</option>
             <option value="5">5</option>
           </select>
